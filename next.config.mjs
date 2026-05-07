54c68d6992f90897837d78114b5c42e6d23abce3
Amaribrahim-1 <amaribrahimforwork1@gmail.com>
2026-05-07 23:32:54 +0300

    feat: integrate Supabase and implement dynamic cabins list
    - Add Supabase client and fetch cabins from database
    - Add Spinner component and implement Suspense for loading states
    - Optimize CabinCard with Next.js Image and Link components
    - Configure Next.js remote patterns for Supabase images

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
     qualities: [75, 100],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ovowiofjdykttridnsaa.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
   },
 };
 
