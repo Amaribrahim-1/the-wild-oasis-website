4f1221596375f80d4425c4383e2adf065c0790f6
Amaribrahim-1 <amaribrahimforwork1@gmail.com>
2026-05-14 23:10:58 +0300

    feat: add cabin reservation system with DateSelector and Reservation components feat: implement UpdateProfileForm for user profile updates style: enhance global styles for month display in date picker fix: add loading state for profile page

---
+++ app/_components/Reservation.js
@@ -0,0 +1,22 @@
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+  return (
+    <div className="border border-primary-800 grid grid-cols-2 min-h-100 p-8">
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
+
+export default Reservation;
