f6440497f916c8471ca60f42efc41fe660eb8fc8
Amaribrahim-1 <amaribrahimforwork1@gmail.com>
2026-05-22 00:15:33 +0300

    feat: integrate authentication with NextAuth and add login message for cabin reservations

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,17 @@
+import { auth } from "@/app/_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import LoginMessage from "./LoginMessage";
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
+
   return (
     <div className="border border-primary-800 grid grid-cols-2 min-h-100 p-8">
       <DateSelector
@@ -14,7 +19,11 @@ async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session?.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
