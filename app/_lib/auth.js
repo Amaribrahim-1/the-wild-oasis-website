80b67df9d454fededab9e71d39b0a641ece2ee8f
Amaribrahim-1 <amaribrahimforwork1@gmail.com>
2026-05-22 06:09:01 +0300

    feat: integrate guest creation and retrieval in authentication flow

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -10,7 +11,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   callbacks: {
     authorized({ auth, request }) {
-      return !!auth?.user;
+      return !!auth?.user; // true or false
+    },
+
+    async signIn({ user }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
 
